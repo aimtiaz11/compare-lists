325060bd0bd3c893e4b64ba24eece8c704f003c0
Ali Imtiaz <a.imtiaz11@gmail.com>
2024-07-15 23:43:23 +1000

    Update compare-list.groovy

--- compare-list.groovy
+++ compare-list.groovy
@@ -1,29 +1,51 @@
+/**
+* Usage: groovy compare-list.groovy first.txt second.txt
+**/
+
 def sourceList = []
-new File( 'input-file1.txt' ).eachLine { line ->
-    sourceList << line.trim()
+
+new File(  args[0] ).eachLine { line ->
+    // do a lowercase after trimming the line.
+    sourceList << line.trim().toLowerCase()
 }
 
 def targetList = []
-new File( 'input-file2.txt' ).eachLine { line ->
-    targetList << line.trim()
+new File( args[1] ).eachLine { line ->
+    targetList << line.trim().toLowerCase()
 }
 
 List diff1 = (sourceList-targetList)
 List diff2 = (targetList-sourceList)
 List diff3 = sourceList.intersect(targetList)
 
-println "Total in sourceList: ${sourceList.size}"
-println "Total in targetList: ${targetList.size}"
 
 
-println "**** Items in sourceList not in targetList list: ${diff1.size} ****"
-diff1.each { println "${it} \n" }
-
-
-println "**** Items in targetList not in sourceList list: ${diff2.size} ****"
-diff2.each { println "${it} \n" }
+println """
+\n
+println "################ COMPARE LIST #########################
+\n
+"""
 
 
 
-println "***** Common items: ${diff3.size} *****"
-diff3.each { println "${it} \n" }
+println "- Total in sourceList: ${sourceList.size}"
+println "- Total in targetList: ${targetList.size}"
+
+
+println "\n\n**** Items in '${args[0]}' not in '${args[1]}' list: ${diff1.size} ****\n"
+diff1.each { println "\t${it}" }
+
+
+println "\n\n**** Items in '${args[1]}' not in '${args[0]}' list: ${diff2.size} ****\n"
+diff2.each { println "\t${it}" }
+
+
+
+println "\n\n***** Common items: ${diff3.size} *****\n"
+
+diff3.each { println "\t${it}" }
+
+println """\n
+######################################################
+\n
+"""
